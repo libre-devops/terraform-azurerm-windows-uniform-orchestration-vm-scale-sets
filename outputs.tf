c2d3cdd59d0d24af1d52283ef0ff7d6cb87f4401
Craig Thacker <craigthackerx@gmail.com>
2024-03-10 03:31:31 +0000

    Update code

--- outputs.tf
+++ outputs.tf
@@ -5,19 +5,19 @@ output "ss_id" {
   description = "The name of the scale set"
 }
 
+output "ss_identity" {
+  value = {
+    for key, value in element(azurerm_windows_virtual_machine_scale_set.windows_vm_scale_set[*], 0) : key => value.identity
+  }
+  description = "The identity block of the scale set"
+}
+
 output "ss_name" {
   value = {
     for key, value in element(azurerm_windows_virtual_machine_scale_set.windows_vm_scale_set[*], 0) : key => value.name
   }
   description = "The name of the scale set"
 }
-
-output "ss_principal_id" {
-#  value = {
-#    for key, value in element(azurerm_windows_virtual_machine_scale_set.windows_vm_scale_set[*], 0) : key => element(value.identity, 0).principal_id
-#  }
-#  description = "Client ID of system assigned managed identity if created"
-#}
 
 output "unique_ss_id" {
   value = {
