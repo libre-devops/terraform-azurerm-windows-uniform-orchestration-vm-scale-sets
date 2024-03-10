12a907eda60329b67610d61983505fa2c61f3422
Craig Thacker <craigthackerx@gmail.com>
2024-03-10 04:36:45 +0000

    Update code

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "rg_name" {
 
 variable "scale_sets" {
   description = "The scale sets list of object variable"
-  type        = list(object({
+  type = list(object({
     name                                              = string
     computer_name_prefix                              = optional(string)
     admin_username                                    = string
@@ -52,7 +52,7 @@ variable "scale_sets" {
     vm_os_simple                                      = optional(string)
     vm_os_sku                                         = optional(string)
     vm_os_version                                     = optional(string)
-    spot_restore                                      = optional(object({
+    spot_restore = optional(object({
       enabled = optional(bool)
       timeout = optional(number)
     }))
@@ -77,13 +77,13 @@ variable "scale_sets" {
       sku       = optional(string)
       version   = optional(string)
     }))
-    single_placement_group      = optional(bool)
-    source_image_id             = optional(string)
-    vtpm_enabled                = optional(bool)
-    zone_balance                = optional(bool)
-    zones                       = optional(list(string))
-    encryption_at_host_enabled  = optional(bool)
-    provision_vm_agent          = optional(bool)
+    single_placement_group     = optional(bool)
+    source_image_id            = optional(string)
+    vtpm_enabled               = optional(bool)
+    zone_balance               = optional(bool)
+    zones                      = optional(list(string))
+    encryption_at_host_enabled = optional(bool)
+    provision_vm_agent         = optional(bool)
     additional_unattend_content = optional(list(object({
       content = string
       setting = string
@@ -117,7 +117,7 @@ variable "scale_sets" {
       secure_vm_disk_encryption_set_id = optional(string)
       security_encryption_type         = optional(string)
       write_accelerator_enabled        = optional(bool)
-      diff_disk_settings               = optional(object({
+      diff_disk_settings = optional(object({
         option    = string
         placement = optional(string)
       }))
@@ -131,23 +131,23 @@ variable "scale_sets" {
       disk_encryption_set_id    = optional(string)
     })))
     extension = optional(list(object({
-      name                              = string
-      publisher                         = string
-      type                              = string
-      type_handler_version              = string
-      auto_upgrade_minor_version        = optional(bool)
-      automatic_upgrade_enabled         = optional(bool)
-      force_update_tag                  = optional(string)
-      provision_after_extensions        = optional(list(string))
-      settings                          = optional(string)
-      protected_settings                = optional(string)
+      name                       = string
+      publisher                  = string
+      type                       = string
+      type_handler_version       = string
+      auto_upgrade_minor_version = optional(bool)
+      automatic_upgrade_enabled  = optional(bool)
+      force_update_tag           = optional(string)
+      provision_after_extensions = optional(list(string))
+      settings                   = optional(string)
+      protected_settings         = optional(string)
       protected_settings_from_key_vault = optional(object({
         secret_url      = optional(string)
         source_vault_id = optional(string)
       }))
     })))
     boot_diagnostics_storage_account_uri = optional(string, null)
-    additional_capabilities              = optional(object({
+    additional_capabilities = optional(object({
       ultra_ssd_enabled = optional(bool)
     }))
     automatic_os_upgrade_policy = optional(object({
@@ -165,7 +165,7 @@ variable "scale_sets" {
       enable_accelerated_networking = optional(bool)
       enable_ip_forwarding          = optional(bool)
       dns_servers                   = optional(list(string))
-      ip_configuration              = optional(list(object({
+      ip_configuration = optional(list(object({
         name                                         = optional(string)
         primary                                      = optional(bool)
         application_gateway_backend_address_pool_ids = optional(list(string))
@@ -174,12 +174,12 @@ variable "scale_sets" {
         load_balancer_inbound_nat_rules_ids          = optional(list(string))
         version                                      = optional(string)
         subnet_id                                    = optional(string)
-        public_ip_address                            = optional(object({
+        public_ip_address = optional(object({
           name                    = optional(string)
           domain_name_label       = optional(string)
           idle_timeout_in_minutes = optional(number)
           public_ip_prefix_id     = optional(string)
-          ip_tag                  = optional(list(object({
+          ip_tag = optional(list(object({
             type = optional(string)
             tag  = optional(string)
           })))
