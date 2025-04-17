b630b554ba74ab6126e470a4d92fed991e6e1a24
Craig Thacker <craigthackerx@gmail.com>
2025-04-17 20:53:43 +0100

    Update code

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,27 @@
 output "ss_id" {
   value = {
-    for key, value in element(azurerm_windows_virtual_machine_scale_set.windows_vm_scale_set[*], 0) : key => value.id
+    for key, value in azurerm_windows_virtual_machine_scale_set.windows_vm_scale_set : key => value.id
   }
   description = "The name of the scale set"
 }
 
 output "ss_identity" {
   value = {
-    for key, value in element(azurerm_windows_virtual_machine_scale_set.windows_vm_scale_set[*], 0) : key => value.identity
+    for key, value in azurerm_windows_virtual_machine_scale_set.windows_vm_scale_set : key => value.identity
   }
   description = "The identity block of the scale set"
 }
 
 output "ss_name" {
   value = {
-    for key, value in element(azurerm_windows_virtual_machine_scale_set.windows_vm_scale_set[*], 0) : key => value.name
+    for key, value in azurerm_windows_virtual_machine_scale_set.windows_vm_scale_set : key => value.name
   }
   description = "The name of the scale set"
 }
 
 output "unique_ss_id" {
   value = {
-    for key, value in element(azurerm_windows_virtual_machine_scale_set.windows_vm_scale_set[*], 0) : key => value.unique_id
+    for key, value in azurerm_windows_virtual_machine_scale_set.windows_vm_scale_set : key => value.unique_id
   }
   description = "The id of the scale set"
 }
